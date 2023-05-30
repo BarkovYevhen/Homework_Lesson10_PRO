7d6440d68b5637618e7db1fd5c9e64b20c9e01e0
Yevhen Barkov <mr.e.barkov@gmail.com>
2023-05-30 23:57:08 +0300

    Ready

---
+++ ghost.js
@@ -0,0 +1,24 @@
+const textArea = document.getElementById("textArea");
+
+textArea.addEventListener("focus", () => {
+  createGhostDiv();
+});
+
+textArea.addEventListener("blur", () => {
+  removeGhostDiv();
+});
+
+function createGhostDiv() {
+  const ghostDiv = document.createElement("div");
+  ghostDiv.id = "ghostDiv";
+  newDiv.setAttribute(innerWidth, 200, innerHeight, 60);
+  ghostDiv.innerText = "Boo! I`m a ghost!";
+  textArea.parentNode.appendChild(ghostDiv);
+}
+
+function removeGhostDiv() {
+  const ghostDiv = document.getElementById("ghostDiv");
+  if (ghostDiv) {
+    ghostDiv.parentNode.removeChild(ghostDiv);
+  }
+}
